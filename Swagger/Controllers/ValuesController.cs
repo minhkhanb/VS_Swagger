d24ae10fcca34650a7f148d9ac12fd7d66cbfc6d
KhaLe <minhkhanb@gmail.com>
2017-08-09 10:28:06 +0700

    - add swagger - add rest api - operation filter for overload

---
+++ Swagger/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Swagger.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        private string[] lista {
+            get {
+                if (System.Web.HttpContext.Current.Application["lista"] == null)
+                {
+                    System.Web.HttpContext.Current.Application["lista"] = new string[] { "value1", "value2" };
+                }
+                return (string[])System.Web.HttpContext.Current.Application["lista"];
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the list of values
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return lista;
+        }
+
+        /// <summary>
+        /// Retrieves one value from the list of values
+        /// </summary>
+        /// <param name=<em>"id"</em>>The id of the item to be retrieved</param>
+        /// <returns></returns>
+        public string Get(int id)
+        {
+            //return "value";
+            return lista[id];
+        }
+
+        /// <summary>
+        /// Insert a new value in the list
+        /// </summary>
+        /// <param name=<em>"value"</em>>New value to be inserted</param>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Change a single value in the list
+        /// </summary>
+        /// <param name=<em>"id"</em>>The id of the value to be changed</param>
+        /// <param name=<em>"value"</em>>The new value</param>
+        public void Put(int id, [FromBody]string value)
+        {
+            lista[id] = value;
+        }
+
+        /// <summary>
+        /// Delete an item from the list
+        /// </summary>
+        /// <param name=<em>"id"</em>>id of the item to be deleted</param>
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ libTools/MultipleOperationsWithSameVerbFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.Swagger;
+using System.Web.Http.Description;
+
+namespace libTools
+{
+    public class MultipleOperationsWithSameVerbFilter:IOperationFilter
+    {
+        public void Apply(
+              Operation operation,
+              SchemaRegistry schemaRegistry,
+              ApiDescription apiDescription)
+        {
+            if (operation.parameters != null)
+            {
+                operation.operationId += "By";
+                foreach (var parm in operation.parameters)
+                {
+                    operation.operationId += string.Format("{0}",
+                        parm.name);
+                }
+            }
+        }
+    }
+}
